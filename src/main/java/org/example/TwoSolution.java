191f50b2802eb982b1412334274e40a2838f1f4f
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-09-12 23:42:05 -0400

    Solution Three

--- src/main/java/org/example/TwoSolution.java
+++ src/main/java/org/example/TwoSolution.java
@@ -1,38 +1,23 @@
 package org.example;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class TwoSolution {
 
     public static void main(String[] args) {
-        int i = 4;
-        double d = 4.0;
-        String s = "HackerRank ";
-
         Scanner scan = new Scanner(System.in);
-
-        /* Declare second integer, double, and String variables. */
-        int x = 12;
-        double y = 4.0;
-        String z = "is the best place to learn and practice coding!";
-
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-
-        /* Print the sum of both integer variables on a new line. */
-        System.out.println("\n"+i+x);
-
-        /* Print the sum of the double variables on a new line. */
-        System.out.println(d+y);
-
-        /* Concatenate and print the String variables on a new line;
-        	the 's' variable above should be printed first. */
-        System.out.println(s+z);
-
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
         scan.close();
+
+        // Calculate Tax and Tip:
+        double tip = mealCost * tipPercent / 100;
+        double tax = mealCost * taxPercent / 100;
+
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = (int) Math.round(mealCost + tax + tip);
+
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
     }
 }
