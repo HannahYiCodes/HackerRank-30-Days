23b734369eadd4dafa300c3c23e05b4514480f20
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-09-13 17:54:05 -0400

    Java Basics

--- src/main/java/org/example/javadaily/OutputFormatting.java
+++ src/main/java/org/example/javadaily/OutputFormatting.java
@@ -5,10 +5,26 @@ public class OutputFormatting {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("================================");
-        for(int i=0;i<3;i++){
-            String s1=sc.next();
-            int x=sc.nextInt();
-            //Complete this line
+
+        for(int i=0;i<3;i++)
+        {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            String xs = "" + x;
+
+            while(xs.length() < 3)
+            {
+                xs = "0" + xs;
+            }
+
+            while(s1.length() < 15)
+            {
+                s1 = s1 + " ";
+            }
+
+            System.out.print(s1);
+            System.out.print(xs);
+            System.out.println();
         }
         System.out.println("================================");
 
