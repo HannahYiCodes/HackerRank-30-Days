e5dc4c7a445fe645acdcaeb821a43a2f229101ab
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-09-21 20:21:47 -0400

    SubString Comparison

--- src/main/java/org/example/javadaily/SubStringCompare.java
+++ src/main/java/org/example/javadaily/SubStringCompare.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 // INCOMPLETE!!!
 public class SubStringCompare {
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
 
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-
+        for(int i=1;i<s.length()-k+1;i++)
+        {
+            if(s.substring(i,i+k).compareTo(smallest)<=0)
+            {
+                smallest=s.substring(i,i+k);
+            }
+            if(s.substring(i,i+k).compareTo(largest)>=0)
+            {
+                largest=s.substring(i,i+k);
+            }
+        }
         return smallest + "\n" + largest;
     }
 
