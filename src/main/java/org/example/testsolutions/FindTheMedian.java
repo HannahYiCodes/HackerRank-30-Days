1bc37b653d701ae316c0088ec35940cc0168f6c0
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-09-29 17:33:12 -0400

    Find the Median

---
+++ src/main/java/org/example/testsolutions/FindTheMedian.java
@@ -0,0 +1,13 @@
+package org.example.testsolutions;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FindTheMedian {
+    public static double findMedian(List<Integer> arr)
+    {
+        Collections.sort(arr);
+        int a= (0+arr.size()-1)/2;
+        return arr.get(a);
+    }
+}
