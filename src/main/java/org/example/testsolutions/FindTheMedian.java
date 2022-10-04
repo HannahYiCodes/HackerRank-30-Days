009b6e9dfd2587a4295733a046e774902b6e4681
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-03 21:08:29 -0400

    REDO without collections

--- src/main/java/org/example/testsolutions/FindTheMedian.java
+++ src/main/java/org/example/testsolutions/FindTheMedian.java
@@ -3,6 +3,7 @@ package org.example.testsolutions;
 import java.util.Collections;
 import java.util.List;
 
+//redo without collections
 public class FindTheMedian {
     public static double findMedian(List<Integer> arr)
     {
