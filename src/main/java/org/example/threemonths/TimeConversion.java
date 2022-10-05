65afd3dde255b8f2d46dbf3b58c7bbe5d263b0bc
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-04 21:49:43 -0400

    Time Conversion

--- src/main/java/org/example/threemonths/TimeConversion.java
+++ src/main/java/org/example/threemonths/TimeConversion.java
@@ -1,14 +1,6 @@
 package org.example.threemonths;
 
 class TimeConversion {
-
-    /*
-     * Complete the 'timeConversion' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts STRING s as parameter.
-     */
-
     public static String timeConversion(String s) {
         // Write your code here
         int hour = Integer.parseInt(s.substring(0, 2));
