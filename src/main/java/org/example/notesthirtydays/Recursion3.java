0d8dd08e5ab69026ec6c41b01aa2401d804a10c3
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-17 19:02:28 -0400

    Recursion 3

--- src/main/java/org/example/notesthirtydays/Recursion3.java
+++ src/main/java/org/example/notesthirtydays/Recursion3.java
@@ -6,18 +6,6 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Recursion3 {
-    abstract class Book {
-        String title;
-        String author;
-
-        Book(String title, String author) {
-            this.title = title;
-            this.author = author;
-        }
-
-        abstract void display();
-    }
-
     public static int factorial(int n) {
         if (n <= 1) {
             return 1;
