bdebff55c11f68cfd5f8a1db0ef8fcdfe8ff3046
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-18 19:39:42 -0400

    Class and Instance

--- src/main/java/org/example/notesthirtydays/ClassInstance.java
+++ src/main/java/org/example/notesthirtydays/ClassInstance.java
@@ -2,41 +2,42 @@ package org.example.notesthirtydays;
 
 public class ClassInstance {
 
-        class Person {
+    class Person {
 
-            int age;
-            int initialAge;
+        int age;
+        int initialAge;
 
 //            Person() {
 //                this.age = 0;
 //                this.initialAge = 0;
 //            }
 
-            Person(int initialAge){
-                this.age = age;
-                this.initialAge = initialAge;
-            }
-
-            int getInitialAge(){
-                return initialAge;
-            }
-
-        public static void amIOld() {
-            String ageStatement;
-
-            if(age >= 18) {
-                ageStatement = "You are old.";
-            } else if (age >= 13 && < 18) {
-                ageStatement = "You are a teenager.";
-            } else if (age > 0 && age <= 12) {
-                ageStatement = "You are young.";
-            } else {
-                ageStatement = "Age is not valid, setting age to 0.\nYou are young.";
-            }
-            System.out.println(ageStatement);
+        Person(int initialAge) {
+            this.age = age;
+            this.initialAge = initialAge;
         }
 
-        public static void yearPasses() {
-            age++;
-        }
+//            int getInitialAge(){
+//                return initialAge;
+//            }
+//
+//        public static void amIOld() {
+//            String ageStatement;
+//
+//            if(age >= 18) {
+//                ageStatement = "You are old.";
+//            } else if (age >= 13 && < 18) {
+//                ageStatement = "You are a teenager.";
+//            } else if (age > 0 && age <= 12) {
+//                ageStatement = "You are young.";
+//            } else {
+//                ageStatement = "Age is not valid, setting age to 0.\nYou are young.";
+//            }
+//            System.out.println(ageStatement);
+//        }
+//
+//        public static void yearPasses() {
+//            age++;
+//        }
     }
+}
