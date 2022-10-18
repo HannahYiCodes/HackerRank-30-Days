1d6e73e84bd0880c65a61700c6824e4b03a5dec4
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-18 19:25:28 -0400

    Binary Numbers

---
+++ src/main/java/org/example/notesthirtydays/BinaryNumbers.java
@@ -0,0 +1,31 @@
+package org.example.notesthirtydays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BinaryNumbers {
+    public static void formula(int n){
+        String binaryCode = Integer.toBinaryString(n);
+        int totalCount = 0;
+        int maxiumCount= 0;
+        for (int i = 0; i < binaryCode.length(); i++) {
+            char character = binaryCode.charAt(i);
+            if (character == '1'){
+                totalCount++;
+                if (maxiumCount < totalCount)
+                    maxiumCount = totalCount;
+            } else {
+                totalCount = 0;
+            }
+        }
+        System.out.println(maxiumCount);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        formula(n);
+        bufferedReader.close();
+    }
+}
