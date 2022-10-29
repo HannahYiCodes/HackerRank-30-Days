4771adc2e17b70482be1d6855501170c3e6bfb4e
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-28 22:09:13 -0400

    Inheritances

---
+++ src/main/java/notesthirtydays/interfaces/Inheritances.java
@@ -0,0 +1,28 @@
+package notesthirtydays.interfaces;
+import java.io.*;
+import java.util.*;
+
+public class Inheritances {
+    interface AdvancedArithmetic{
+        int divisorSum(int n);
+    }
+    class Calculator implements AdvancedArithmetic {
+        public int divisorSum(int n) {
+            return 0;
+        }
+    }
+
+    class Solution {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+            scan.close();
+
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+            System.out.println(sum);
+        }
+    }
+}
