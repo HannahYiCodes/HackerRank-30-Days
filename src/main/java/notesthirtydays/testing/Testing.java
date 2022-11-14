1d11414a2caf9fbbc7cb2e81eb9d5679a15c6b80
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-11-13 23:30:02 -0500

    Testing

---
+++ src/main/java/notesthirtydays/testing/Testing.java
@@ -0,0 +1,13 @@
+package notesthirtydays.testing;
+
+public class Testing {
+    // EXAMPLE!!!
+//    def minimum_index(seq):
+//            if len(seq) == 0:
+//    raise ValueError("Cannot get the minimum value index from an empty sequence")
+//    min_idx = 0
+//            for i in range(1, len(seq)):
+//            if a[i] < a[min_idx]:
+//    min_idx = i
+//    return min_idx
+}
