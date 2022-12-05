f494b8e35275881ab6d754bdec3be72e63748546
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-12-04 19:21:16 -0500

    Abstract Classes

--- src/main/java/notesthirtydays/abstractclasses/AbstractClasses.java
+++ src/main/java/notesthirtydays/abstractclasses/AbstractClasses.java
@@ -33,9 +33,34 @@ public class AbstractClasses {
      * Print the title, author, and price in the specified format.
      **/
     // Write your method here
+    class MyBook extends Book{
+        private int price;
+
+        public MyBook(String title, String author,int price){
+            super( title,  author);
+            this.price = price;
+        }
+
+        void display(){
+            System.out.println("Title: "+ title);
+            System.out.println("Author: "+author);
+            System.out.println("Price: "+ price );
+        }
+
+    }
 
 // End class
 
     public class Solution {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+        }
     }
 }
