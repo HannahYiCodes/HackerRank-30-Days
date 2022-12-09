7af7b0e6dabaff58946639e43cd1c1870214794b
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-12-08 19:01:12 -0500

    String to Integer Comments

--- src/main/java/notesthirtydays/stringtointeger/StringToInteger.java
+++ src/main/java/notesthirtydays/stringtointeger/StringToInteger.java
@@ -27,3 +27,20 @@ public class StringToInteger {
         }
     }
 }
+
+//public class Solution {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        String S = bufferedReader.readLine();
+//
+//        try{
+//            int i = Integer.parseInt(S);
+//            System.out.println(i);
+//        }catch(Exception e){
+//            System.out.println("Bad String");
+//        }
+//
+//        bufferedReader.close();
+//    }
+//}
