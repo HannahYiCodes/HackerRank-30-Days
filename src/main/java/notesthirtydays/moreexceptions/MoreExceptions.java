84177a1ce9b3b92cbab7936d37910ec6c1b31e47
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-12-09 20:25:58 -0500

    More Exceptions

--- src/main/java/notesthirtydays/moreexceptions/MoreExceptions.java
+++ src/main/java/notesthirtydays/moreexceptions/MoreExceptions.java
@@ -27,3 +27,14 @@ public class MoreExceptions {
         }
     }
 }
+
+// Another Solution
+//class Calculator {
+//    public int power(int n, int p) throws Exception {
+//        if(n < 0 || p < 0) throw new Exception("n and p should be non-negative");
+//        int power = 1;
+//        for(int i = 0; i < p; i++)
+//            power = power * n;
+//        return power;
+//    }
+//}
