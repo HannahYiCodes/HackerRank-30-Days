b59e7be8208601fa06eac41d9c9808743c6f5cdd
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-12-19 23:59:44 -0500

    Testing

--- src/main/java/notesthirtydays/testing/Testing.java
+++ src/main/java/notesthirtydays/testing/Testing.java
@@ -10,4 +10,44 @@ public class Testing {
 //            if a[i] < a[min_idx]:
 //    min_idx = i
 //    return min_idx
-}
+// }
+
+// Another answer
+//static class TestDataEmptyArray{
+//
+//    public static int[] get_array(){
+//        int[] a = {};
+//        return a;
+//    }
+//}
+//static class TestDataUniqueValues{
+//    private static int[] a = {1,2};
+//
+//    public static int[] get_array(){
+//        return a;
+//    }
+//
+//    public static int get_expected_result(){
+//        int minIndex =0;
+//        for(int i=0; i<a.length;i++){
+//            if(a[i]<a[minIndex]) minIndex=1;
+//        }
+//        return minIndex;
+//    }
+//}
+//
+//static class TestDataExactlyTwoDifferentMinimums{
+//    public static int[] a = {1,1};
+//
+//    public static int[] get_array(){
+//        return a;
+//    }
+//
+//    public static int get_expected_result(){
+//        int minIndex =0;
+//        for(int i=0; i<a.length;i++){
+//            if(a[i]<a[minIndex]) minIndex=1;
+//        }
+//        return minIndex;
+//    }
+//}
